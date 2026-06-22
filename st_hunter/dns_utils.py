b04fbf208c1561353e2c41775e10a872b10338ac
ghoxtbyte <52350126+ghoxtbyte@users.noreply.github.com>
2026-06-22 17:28:54 +0330

    Update dns_utils.py

--- st_hunter/dns_utils.py
+++ st_hunter/dns_utils.py
@@ -1,22 +1,26 @@
 import asyncio
 import sys
+import aiodns
 from .output import output_lines, print_status_line
 
-async def dig_full(domain, rtype, dns_server=None):
+async def get_ns_records(domain):
+    
     try:
-        cmd = ["dig"]
-        if dns_server:
-            cmd.append(f"@{dns_server}")
-        cmd += [domain, rtype, "+noshort", "+noquestion", "+noauthority", "+noadditional", "+comments"]
+        resolver = aiodns.DNSResolver()
+        res = await resolver.query(domain, 'NS')
+        return [ns.host.rstrip('.') for ns in res]
+    except Exception:
+        return []
+
+async def _dig_axfr_raw(domain, dns_server):
+    
+    try:
+        cmd = ["dig", f"@{dns_server}", domain, "AXFR", "+noshort", "+noquestion", "+noauthority", "+noadditional", "+comments"]
         proc = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.DEVNULL)
         stdout, _ = await proc.communicate()
         return stdout.decode()
     except Exception:
         return ""
-
-async def get_ns_records(domain):
-    ns_output = await dig_full(domain, "NS")
-    return [line.split()[-1].rstrip('.') for line in ns_output.splitlines() if "\tNS\t" in line]
 
 async def perform_axfr(domain, ns_records, silent_mode):
     discovered_subs = set()
@@ -27,7 +31,7 @@ async def perform_axfr(domain, ns_records, silent_mode):
         return discovered_subs
         
     for ns in ns_records:
-        axfr_output = await dig_full(domain, "AXFR", ns)
+        axfr_output = await _dig_axfr_raw(domain, ns)
         
         records = [ln for ln in axfr_output.splitlines() if "\tIN\t" in ln or " IN " in ln]
         if records:
@@ -38,7 +42,6 @@ async def perform_axfr(domain, ns_records, silent_mode):
             for ln in records:
                 if not silent_mode:
                     print(f"    {ln}")
-                
                 
                 parts = ln.split()
                 if parts:
